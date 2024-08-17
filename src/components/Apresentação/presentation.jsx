ad079be1fb5c30a9208fdf965758868add3d47cd
João Stadler <joaogbstadler@gmail.com>
2024-08-17 09:00:08 -0300

    Seção de apresentação adicionada

--- src/components/Apresentação/presentation.jsx
+++ src/components/Apresentação/presentation.jsx
@@ -1,10 +1,15 @@
 import PresentationText from "./presentationTXT";
+import FotoPostInsta from "../../images/foto-post-instagran.png"
 import "./styles/presentation.css"
 
 export default function Presentation(){
     return(
-        <section>
+        <section className="presentation d-flex align-items-center justify-content-around">
             <PresentationText/>
+            <figure>
+                <img src={FotoPostInsta} alt="Foto Bea menu de apresentação" 
+                className="insta-post-image"/>
+            </figure>
         </section>
     )
 }
