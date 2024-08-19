1b5daa03416852899a4e037ce0aad5c8e4285493
João Stadler <joaogbstadler@gmail.com>
2024-08-19 17:01:06 -0300

    Imagem da pagina de email adicionada

--- src/components/contactar/contact.jsx
+++ src/components/contactar/contact.jsx
@@ -1,12 +1,15 @@
-import { MdEmail } from "react-icons/md";
 import "./styles/contact.css";
+import BeaImageEmail from "../../images/foto-bea-email-page.jpg";
+import EmailIcon from "../../images/email-icon.png";
 
 export default function Contact(){
     return(
-        <section className="contact text-white text-center d-flex
-        flex-column justify-content-around align-items-center">
-                <h1 className="text-upppercase">Deseja me contratar para uma parceria?</h1>
-                <a href="#" className="email-link"><MdEmail/></a>
+        <section className="contact text-white">
+                <h1 className="text-upppercase text-center">Deseja me contratar para uma parceria?</h1>
+                <img src={BeaImageEmail} className="bea-img-left-side"/>
+                <a href="#" className="email-link">
+                    <img src={EmailIcon} className="email-icon"/>
+                </a>
         </section>
     );
 }
