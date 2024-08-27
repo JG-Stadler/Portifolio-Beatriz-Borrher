f043b6804567d8d08d3b728aea723bb69a741ef1
João Stadler <joaogbstadler@gmail.com>
2024-08-27 14:10:51 -0300

    Opções de navegação removidas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./index.css";
 import Presentation from "./components/Apresentação/presentation";
-import MainMenu from "./components/main-menu/mainMenu";
 import About from "./components/Sobre/about";
 import SocialMidias from "./components/redes-sociais/socialMidia";
 import Contact from "./components/contactar/contact.jsx";
@@ -9,10 +8,7 @@ import Footer from "./components/rodapé/footer.jsx";
 function App() {
   return (
     <div className="App">
-      <main className="begin-page">
-         <MainMenu/>
-        <Presentation/>
-      </main>
+      <Presentation/>
       <About/>
       <SocialMidias/>
       <Contact/>
--- src/components/rodapé/footer.jsx
+++ src/components/rodapé/footer.jsx
@@ -1,14 +1,12 @@
 import FooterLogo from "../../images/Logo_Bea-branca-fundo_transparente.png";
 import { FaInstagram } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
-import FooterNav from "./footerNav";
 import "./styles/footer.css";
 
 export default function Footer(){
     return(
     <footer className="page-end-footer text-white">
         <img src={FooterLogo} className="footer-logo"/>
-        <FooterNav/>
         <nav className="social-midia-links-ft d-flex
         align-items-center">
             <a href="
